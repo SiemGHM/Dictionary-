363ccc7e5daa0bd46b535fd3021d682be90a2e84
SiemGHM <SiemGHirmai2@gmail.com>
2020-10-06 19:37:44 +0400

    The appearance not changed. The HTML, CSS, JS embedded into the python file, working smoothly.

--- back.py
+++ back.py
@@ -1,3 +1,19 @@
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, request, url_for, session
+
 
 app=Flask(__name__)
+app.secret_key="heythere"
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+    
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
