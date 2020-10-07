74343c5fe977103c65ec0e690c87e19e30d79e39
SiemGHM <SiemGHirmai2@gmail.com>
2020-10-07 23:02:31 +0400

    This commit is the commit of a fully finished external design embeded in the python file back.py

---
+++ test.py
@@ -0,0 +1,9 @@
+for i in range(13):
+    for j in range(18):
+        print(" " *j,end="")
+        print('Joke is on you')
+
+    for j in range(30,0,-1):
+        print(" " *j,end="")
+        print('lol')
+
