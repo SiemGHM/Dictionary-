277f7d787f25ec0059ac20cc5330e8c3472ab926
SiemGHM <SiemGHirmai2@gmail.com>
2020-10-17 02:58:28 +0400

    All the features of the website are working with the exeption of the additional web pages. The pronounciation is showing. The links need some work.

--- back.py
+++ back.py
@@ -3,12 +3,13 @@ import requests
 import json
 import random
 
-app_id = '46f8fc36'
-app_key = '43adc50a2f3ba559f46f7ab46176eed1'
+app_id = '5447e8b7'
+app_key = '368b6762ad54758733d7d3a8875ed889'
 
 language = 'en-gb'
 #word_id = 'prone'
 fields = 'definitions'
+fields2 = 'pronunciations'
 strictMatch = 'false'
 words={'word1':[],'word2':[],'word3':[],'word4':[],'word5':[],'word6':[],'word7':[],'word8':[],'word9':[],'word10':[]}
 W1 = 0 
@@ -31,8 +32,31 @@ M7 = 0
 M8 = 0 
 M9 = 0 
 M10 = 0 
+WC1 = 0 
+WC2 = 0 
+WC3 = 0 
+WC4 = 0 
+WC5 = 0 
+WC6 = 0 
+WC7 = 0 
+WC8 = 0 
+WC9 = 0 
+WC10 = 0
+P1 = 0 
+P2 = 0 
+P3 = 0 
+P4 = 0 
+P5 = 0 
+P6 = 0 
+P7 = 0 
+P8 = 0 
+P9 = 0 
+P10 = 0  
 Ws=[W1, W2, W3, W4, W5, W6, W7, W8, W9, W10]
 Ms=[M1, M2, M3, M4, M5, M6, M7, M8, M9, M10]
+WC=[WC1,WC2,WC3,WC4,WC5,WC6,WC7,WC8,WC9,WC10]
+P=[P1 ,P2 ,P3 ,P4 ,P5 ,P6 ,P7 ,P8 ,P9 ,P10]
+
 
 app=Flask(__name__)
 app.secret_key="heythere"
@@ -59,17 +83,44 @@ def getWord():
     randWord=wordList[(randnum-1)]
     word_id= randWord
     url = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/' + language + '/' + word_id.lower() + '?fields=' + fields + '&strictMatch=' + strictMatch;
+    
     r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key})
+    
     answer=r.text
-    rescode=str(r.status_code).strip()
-    print(rescode)
-    if rescode=='200':
-        res = json.loads(answer)
+    
+    rescode=int(str(r.status_code).strip())
+    res = json.loads(answer)
+    
+    
+    
+    #print(answer2)
+    url2 = 'https://od-api.oxforddictionaries.com:443/api/v2/entries/' + language + '/' + word_id.lower() + '?fields=' + fields2 + '&strictMatch=' + strictMatch;
+    r2 = requests.get(url2, headers = {'app_id': app_id, 'app_key': app_key})
+    answer2=r2.text
+    rescode2=int(str(r2.status_code).strip())
+    res2=json.loads(answer2)
+    sumrescode=(rescode+rescode2)
+    print(sumrescode)
+    
+    
+    if sumrescode==400:
+        print('1')
+        
+        
         print(word_id)
+        
+        
+        #print(answer2)
+        print('2')
         try:
-            res=res["results"][0]["lexicalEntries"][0]['entries'][0]['senses'][0]['definitions'][0]
-            print(randWord,res)
-            wM=[randWord,res]
+            resp=res["results"][0]["lexicalEntries"][0]['entries'][0]['senses'][0]['definitions'][0]
+            print('4')
+            WordC=res["results"][0]["lexicalEntries"][0]['lexicalCategory']['text']
+            pron=res2["results"][0]["lexicalEntries"][0]['entries'][0]['pronunciations'][0]['phoneticSpelling']
+            print("5")
+            print(randWord,resp, WordC,pron)
+            wM=[randWord,resp,WordC,pron]
+            print('3')
             return wM
         except:
             return getWord()
@@ -106,6 +157,9 @@ n=0
 for w in words:
     Ws[n]=words[w][0]
     Ms[n]=words[w][1]
+    WC[n]=words[w][2]
+    P[n]=words[w][3]
+    
     n+=1
 
 
@@ -117,10 +171,12 @@ for w in words:
 
 @app.route("/")
 def home():
-    return render_template("index.html",W1 = Ws[0], W2 = Ws[1], W3 = Ws[2], W4 = Ws[3], W5 = Ws[4], W6 = Ws[5], W7 = Ws[6], W8 = Ws[7], W9 = Ws[8], W10 = Ws[9], M1 = Ms[0], M2 = Ms[1], M3 = Ms[2], M4 = Ms[3], M5 = Ms[4], M6 = Ms[5], M7 = Ms[6], M8 = Ms[7], M9 = Ms[8], M10 = Ms[9])
+    return render_template("index.html",W1 = Ws[0], W2 = Ws[1], W3 = Ws[2], W4 = Ws[3], W5 = Ws[4], W6 = Ws[5], W7 = Ws[6], W8 = Ws[7], W9 = Ws[8], W10 = Ws[9], M1 = Ms[0], M2 = Ms[1], M3 = Ms[2], M4 = Ms[3], M5 = Ms[4], M6 = Ms[5], M7 = Ms[6], M8 = Ms[7], M9 = Ms[8], M10 = Ms[9],WC1 = WC[0], WC2 = WC[1], WC3 = WC[2], WC4 = WC[3], WC5 = WC[4], WC6 = WC[5], WC7 = WC[6], WC8 = WC[7], WC9 = WC[8], WC10 = WC[9], P1  = P[0], P2  = P[1], P3  = P[2], P4  = P[3], P5  = P[4], P6  = P[5], P7  = P[6], P8  = P[7], P9  = P[8], P10  = P[9])
     
 
-
+@app.route('/IPA')
+def ipa():
+    return render_template("IPA.html")
 
 
 
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ with open('temp.txt','w') as open:
     n=1
     x=0
     for i in range(10):
-        print('M'+str(n)+' = '+'Ms['+str(x)+"], ",file=open, end='' )
+        print('P'+str(n)+'  = '+'P['+str(x)+"], ",file=open, end='' )
         n+=1
         x+=1
 
@@ -10,5 +10,8 @@ with open('temp.txt','w') as open:
 
     n=1
     for i in range(10):
-        print('M'+str(n)+' = 0 ',file=open,  )
+        print('P'+str(n)+' = 0 ',file=open,  )
         n+=1
+
+
+n=50
